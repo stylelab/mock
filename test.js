b5fcb5ba05d0185e5055bd27f316fe48499435de
ACN Taku Iwahashi <taku.iwahashi@accenture.com>
2023-10-30 13:27:40 +0900

    no message

--- test.js
+++ test.js
@@ -25,33 +25,128 @@ tag.src = 'https://www.youtube.com/iframe_api'
 let firstScriptTag = document.getElementsByTagName('script')[0]
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 let player
+
+let players = []
+let movieThumbs = null
 function onYouTubePlayerAPIReady() {
-  player = new YT.Player('player', {
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange,
-    },
+  $('.youtube').each(function (index) {
+    var id = this.id
+    player = new YT.Player(id, {
+      events: {
+        //onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    })
+    players.push(player)
+  })
+
+  //THM参照取得＋イベント設定
+  movieThumbs = $('.movie-thumb')
+  movieThumbs.each(function (index) {
+    $(this).on('click', function () {
+      onThmClick(index)
+    })
+  })
+  //   var mp = new MyPlayer('player_01')
+}
+
+//再生
+onThmClick = function (index) {
+  console.log(index)
+  players[index].playVideo()
+}
+
+//再生状態変化
+onPlayerStateChange = function (e) {
+  let targetMovieID = e.target.g.id
+
+  //movieIdと同じdata-idを持つボタンを制御する
+  movieThumbs.each(function () {
+    var btnID = $(this).data('link-id')
+    if (btnID == targetMovieID) {
+      if (e.data == 0 || e.data == 2) {
+        $(this).show()
+      } else {
+        $(this).hide()
+      }
+    }
   })
 }
 
-function onPlayerReady(event) {
-  let playButton = document.getElementById('play')
-  let el = document.getElementsByClassName('videoWrapper')
-  playButton.addEventListener('click', function () {
-    player.playVideo()
-    playButton.style.display = 'none'
-    el[0].classList.add('pv')
-  })
-}
+// let players = []
+// function onYouTubePlayerAPIReady() {
+//   player = new YT.Player('player_01', {
+//     events: {
+//       onReady: (function () {
+//         onPlayerReady('play')
+//       })(),
+//       onStateChange: onPlayerStateChange,
+//     },
+//   })
+//   //players.push(player)
+// }
+
+// //
+// var MyPlayer = function (playerID) {
+//   this.init(playerID)
+// }
+// MyPlayer.prototype = {
+//   playButton: null,
+//   player: null,
+//   init: function (playerID) {
+//     var self = this
+//     this.player = new YT.Player(playerID, {
+//       events: {
+//         //onReady: this.onPlayerReady,
+//         onReady: (function () {
+//           self.onPlayerReady()
+//         })(),
+//         onStateChange: this.onPlayerStateChange,
+//       },
+//     })
+//     this.playButton = $('#play')
+//     this.playButton.on('click', function (self) {
+//       //self.player.playVideo()
+//     })
+//   },
+//   test: function (self) {
+//     console.log(self)
+//   },
+//   onPlayerReady: function () {
+//     var self = this
+//     setTimeout(function () {
+//       console.log(self.player)
+//       self.player.playVideo()
+//     }, 10)
+//     //self.player.playVideo()
+//     // this.player.playVideo()
+//     // this.playButton.style.display = 'none'
+//     // $(this.playButton).click = function () {
+//     //   _this.player.playVideo()
+//     //   //_this.playButton.style.display = 'none'
+//     // }
+//   },
+//   onPlayerStateChange: function () {
+//     console.log('BB')
+//   },
+// }
+
+// function onPlayerReady(btnID) {
+//   let playButton = document.getElementById(btnID)
+//   playButton.addEventListener('click', function () {
+//     players[0].playVideo()
+//     playButton.style.display = 'none'
+//   })
+// }
 
 function onPlayerStateChange(e) {
-  let playButton = document.getElementById('play')
-  console.log(e)
-  console.log(e.data)
-  if (e.data == 0 || e.data == 2) {
-    //0:停止 ,2:一時停止
-    playButton.style.display = 'block'
-  } else {
-    playButton.style.display = 'none'
-  }
+  //   let playButton = document.getElementById('play')
+  //   console.log(e)
+  //   console.log(e.data)
+  //   if (e.data == 0 || e.data == 2) {
+  //     //0:停止 ,2:一時停止
+  //     playButton.style.display = 'block'
+  //   } else {
+  //     playButton.style.display = 'none'
+  //   }
 }
