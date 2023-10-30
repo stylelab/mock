07d0f508bb72dcdca15109d59fe8dacd11624732
ACN Taku Iwahashi <taku.iwahashi@accenture.com>
2023-10-30 10:58:45 +0900

    no message

--- test.js
+++ test.js
@@ -17,4 +17,28 @@ $(function () {
       '*'
     )
   }
+
+  //
+  var videoId = 'eW1T1-MHCFw' //動画ID
+  var videoWidth = '640' //動画横サイズ
+  var videoHeight = '360' //動画縦サイズ
+
+  // iframe Player APIを非同期で読み込み
+  var tag = document.createElement('script')
+  tag.src = 'https://www.youtube.com/iframe_api'
+  var firstScriptTag = document.getElementsByTagName('script')[0]
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+  // #player にiframeplayerを作成
+  var player
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: videoHeight,
+      width: videoWidth,
+      videoId: videoId,
+      playerVars: {
+        autoplay: 1, //自動再生する
+      },
+    })
+  }
 })
